b2791f9afbbc30d3c6f5683a2889a3259251d401
Inyeop <seeker1204@naver.com>
2022-04-15 16:41:48 +0900

    네 번째 작업

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
-import os
+from importlib.resources import path
+import os, pickle
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -15,7 +18,14 @@ def mnist() :
         f = request.files['mnistfile']
         path = os.path.dirname(__file__) + '/upload/' + f.filename
         f.save(path)
-        return '성공!!'
+        img = Image.open(path).convert("L")
+        img = np.resize(img,(1,784))
+        # img = 255 - (img)
+        mpath = os.path.dirname(__file__)+'/model1.pickle'
+        with open(mpath,'rb') as f:
+            model = pickle.load(f)
+        pred = model.predict(img)
+        return render_template('mnistresult.html',data=pred)
 
 if __name__ == '__main__' :
     app.run(debug = True)
