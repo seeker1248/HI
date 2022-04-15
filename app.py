cc79e6f2bb94de79abb728532b5bcba4d76272e8
Inyeop <seeker1204@naver.com>
2022-04-15 16:22:30 +0900

    세 번째 작업

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def index() :
     return render_template('index.html')
 
+@app.route('/mnist', methods=['GET', 'POST'])
+def mnist() :
+    if request.method == 'GET' :
+        return render_template('mnistform.html')
+    else :
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__) + '/upload/' + f.filename
+        f.save(path)
+        return '성공!!'
+
 if __name__ == '__main__' :
     app.run(debug = True)
